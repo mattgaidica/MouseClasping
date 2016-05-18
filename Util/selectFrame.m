5e86338d45d114ea25daec7ffaec35f74e54817f
Matt Gaidica <matt@gaidi.ca>
2016-05-18 12:17:59 +0300

    Can select color frame, working on frame details/info

---
+++ Util/selectFrame.m
@@ -0,0 +1,23 @@
+function frameTime = selectFrame(videoFile,nn)
+displaySize = 720; %px
+
+v = VideoReader(videoFile);
+nFrames = v.Duration * v.FrameRate;
+frameSpace = round(linspace(1,nFrames-1,nn*nn));
+allFrame = [];
+for ii=1:nn*nn
+    v.CurrentTime = frameSpace(ii) / v.FrameRate;
+    frame = readFrame(v);
+    frameScale = (displaySize/nn)/size(frame,2);
+    frame = imresize(frame,frameScale);
+    frame = insertText(frame,[1 1],num2str(frameSpace(ii)));
+    allFrames(:,:,:,ii) = frame;
+end
+h = figure;
+montage(allFrames,'Size',[nn nn]);
+framePos = ginput(1);
+frameX = ceil(framePos(1) / size(frame,2));
+frameY = ceil(framePos(2) / size(frame,1));
+frameNumber = frameSpace(((frameY - 1) * nn) + frameX);
+frameTime = frameNumber / v.FrameRate;
+close(h);
--- detectClasps.m
+++ detectClasps.m
@@ -1,50 +1,42 @@
 function detectClasps(videoFile)
-nFrames = 1;
-padFactor = 5;
+threshStd = 5;
 videoWidth = 720;
-rectHeight = 50;
-rectSize = [videoWidth rectHeight];
+rectSize = [50 50];
 
-videoFileReader = vision.VideoFileReader(videoFile);
+v = VideoReader(videoFile);
 
 hblob = vision.BlobAnalysis('AreaOutputPort',true,...
     'CentroidOutputPort',true,... 
     'BoundingBoxOutputPort',true);
 
-% circle active zone
-% % frame = step(videoFileReader);
-% % h = figure;
-% % imshow(frame);
-% % activeRect = round(getPosition(imrect));
-% % close(h);
+frameTime = selectFrame(videoFile,5);
+v.CurrentTime = frameTime;
+frame = readFrame(v);
+v.CurrentTime = 0;
 
-f1_thresholds = getThresholds(videoFile,nFrames,padFactor);
-f1_hsvRect = makeHsvRect(f1_thresholds,rectSize);
+pos = markBody(frame);
+hsvData = getHsvData(frame,pos);
 
-f2_thresholds = getThresholds(videoFile,nFrames,padFactor);
-f2_hsvRect = makeHsvRect(f2_thresholds,rectSize);
+% second ROI
+f1_thresholds = formatThresholds(squeeze(hsvData(2,:,:)),threshStd);
+f1_rect = im2uint8(hsv2rgb(makeHsvRect(f1_thresholds,rectSize)));
+% third ROI
+f2_thresholds = formatThresholds(squeeze(hsvData(3,:,:)),threshStd);
+f2_rect = im2uint8(hsv2rgb(makeHsvRect(f2_thresholds,rectSize)));
 
 initLoop = true;
-while ~isDone(videoFileReader)
-    frame = step(videoFileReader);
+while hasFrame(v)
+    frame = readFrame(v);
     videoScale = videoWidth / size(frame,2);
     frame = imresize(frame,videoScale);
+    frame = insertRect(frame,f1_rect,[10 10]);
     
     if initLoop
         initLoop = false;
-        figureHeight = size(frame,1) + 2*rectHeight;
-        h = figure('position',[0 0 size(frame,2) figureHeight]);
-        hs(1) = subplot(3,1,1);
-        setpixelposition(hs(1),[0 figureHeight-rectHeight videoWidth rectHeight]);
-        imshow(hsv2rgb(f1_hsvRect));
-        hs(2) = subplot(3,1,2);
-        setpixelposition(hs(2),[0 figureHeight-rectHeight*2 videoWidth rectHeight]);
-        imshow(hsv2rgb(f2_hsvRect));
-        hs(3) = subplot(3,1,3);
-        setpixelposition(hs(3),[0 0 size(frame,2) size(frame,1)]);
+        figureHeight = size(frame,1);
+        h = figure('position',[0 0 size(frame)]);
     end
     
-% %     frame = imcrop(frame,activeRect);
     f1_mask = claspMask(frame,f1_thresholds);
     f2_mask = claspMask(frame,f2_thresholds);
     
@@ -55,22 +47,63 @@ while ~isDone(videoFileReader)
     
     if ~isempty(f1_areaKey)
         frame = insertObjectAnnotation(frame,'rectangle', ...
-                    f1_bbox(f1_areaKey,:),'1');
+                    f1_bbox(f1_areaKey,:),'f1');
     end
     if ~isempty(f2_areaKey)
         frame = insertObjectAnnotation(frame,'rectangle', ...
-                    f2_bbox(f2_areaKey,:),'2');
+                    f2_bbox(f2_areaKey,:),'f2');
     end
     if ~isempty(f1_centroid) && ~isempty(f2_centroid)
         frame = insertShape(frame,'Line',[f1_centroid(f1_areaKey,:) f2_centroid(f2_areaKey,:)]);
+        lineCenter = (f1_centroid(f1_areaKey,:) + f2_centroid(f2_areaKey,:)) / 2;
+        lineDist = round(pdist([f1_centroid(f1_areaKey,:);f2_centroid(f2_areaKey,:)]));
+        frame = insertText(frame,lineCenter + [10 -5],strcat(num2str(lineDist),' px'));
     end
     
     imshow(frame);
+    pause(1);
 end
 
 end
 
-function mask=claspMask(frame,thresholds)
+function frame = insertRect(frame,rect,pos)
+% pos = [x,y] from top-left
+frame(pos(1):pos(1)+size(rect,1)-1, pos(2):pos(2)+size(rect,2)-1,:) = rect;
+end
+
+function hsvData = getHsvData(frame,pos)
+% hsvData: dim1=ROI,dim2=[mean,std],dim3=[h,s,v]
+for ii=1:size(pos,1)
+    frameRoi = imcrop(frame,pos(ii,:));
+    frameRoi = rgb2hsv(frameRoi);
+    for jj=1:3
+        hsvData(ii,1,jj) = mean2(frameRoi(:,:,jj));
+        hsvData(ii,2,jj) = std2(frameRoi(:,:,jj));
+    end
+end
+end
+
+function pos = markBody(frame)
+    h = figure;
+    imshow(frame);
+    
+    h1 = imrect(gca);
+    pos(1,:) = wait(h1);
+    setColor(h1,'k');
+    
+    h2 = imrect(gca);
+    pos(2,:) = wait(h2);
+    setColor(h2,'r');
+    
+    h3 = imrect(gca);
+    pos(3,:) = wait(h3);
+    setColor(h3,'r');
+    
+    close(h);
+    pause(1);
+end
+
+function mask = claspMask(frame,thresholds)
 hsvFrame = rgb2hsv(frame);
 mask = HSVthreshold(hsvFrame,thresholds);
 mask = imopen(mask, strel('rectangle', [3,3]));
@@ -78,11 +111,9 @@ mask = imclose(mask, strel('rectangle', [15,15]));
 mask = imfill(mask, 'holes');
 end
 
-function thresholds=getThresholds(videoFile,nFrames,padFactor)
-hsvBounds = getHsvBounds(videoFile,nFrames);
-thresholds = [
-    mean(hsvBounds{1}), std(hsvBounds{1}) * padFactor...
-    mean(hsvBounds{2}), std(hsvBounds{2}) * padFactor...
-    mean(hsvBounds{3}), std(hsvBounds{3}) * padFactor...
-];
+function thresholds = formatThresholds(hsvData,threshStd)
+thresholds = [];
+for ii=1:3
+    thresholds = horzcat(thresholds,[hsvData(1,ii) hsvData(2,ii)*threshStd]);
+end
 end
